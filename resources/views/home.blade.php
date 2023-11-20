51437df0a2e26dd20ab9ee1ad5d47ce6e77e531d
FraRussotto <russottofrancesco1@gmail.com>
2023-11-20 18:14:03 +0100

    [edit] foreach movies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,15 +2,20 @@
 
 @section('content')
     <div class="container d-flex flex-wrap">
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div cLass="card-body d-flex flex-column">
-                <h5 class="card-title">Title</h5>
-                <span>Original title</span>
-                <span>Nationality</span>
-                <span>Data</span>
-                <span>Voto IMDB</span>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+
+        @foreach ($movies as $movie)
+            <div class="card m-2" style="width: 18rem;">
+                <img src="" class="card-img-top" alt="">
+                <div cLass="card-body d-flex flex-column justify-content-between">
+                    <h4 class="card-title">{{ $movie->title }}</h4>
+                    <span>Titolo originale: {{ $movie->original_title }}</span>
+                    <span>Nazionalità: {{ $movie->nationality }}</span>
+                    <span>Uscita: {{ $movie->date }}</span>
+                    <span>Voto: {{ $movie->vote }}</span>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
             </div>
-        </div>
-    @endsection
+        @endforeach
+
+    </div>
+@endsection

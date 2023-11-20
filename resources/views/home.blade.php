315aff7af11b3bff0e5f1490432d4a157cfd7537
FraRussotto <russottofrancesco1@gmail.com>
2023-11-20 17:42:07 +0100

    [add] Controller + Models

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.main')
 
 @section('content')
-    <p class="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, necessitatibus dolorem asperiores ipsam
-        recusandae
-        hic nulla pariatur ea beatae, voluptatibus rem mollitia doloremque a magni voluptatum nesciunt dicta dolores facere
-        possimus expedita. Dolore, quia. Totam nobis consectetur saepe dolorem laborum aperiam veniam explicabo veritatis
-        dignissimos minus! E</p>
-@endsection
+    <div class="container d-flex flex-wrap">
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div cLass="card-body d-flex flex-column">
+                <h5 class="card-title">Title</h5>
+                <span>Original title</span>
+                <span>Nationality</span>
+                <span>Data</span>
+                <span>Voto IMDB</span>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    @endsection

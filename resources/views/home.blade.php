3c64b467449e23688f7594af265178312c2add52
FraRussotto <russottofrancesco1@gmail.com>
2023-11-20 17:17:21 +0100

    [edit] blade main - header - home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+    <p class="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, necessitatibus dolorem asperiores ipsam
+        recusandae
+        hic nulla pariatur ea beatae, voluptatibus rem mollitia doloremque a magni voluptatum nesciunt dicta dolores facere
+        possimus expedita. Dolore, quia. Totam nobis consectetur saepe dolorem laborum aperiam veniam explicabo veritatis
+        dignissimos minus! E</p>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,3 @@
+<header>
+    <h1>I miei film</h1>
+</header>
